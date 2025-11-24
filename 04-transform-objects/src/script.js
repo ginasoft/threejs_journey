c9048eb58825cea58fc4c97bc2e592d6c2779cd1
Natalia Cuellas <nataliagcuellas@gmail.com>
2025-11-24 19:45:27 -0300

    position, scale and rotation exercises

--- 04-transform-objects/src/script.js
+++ 04-transform-objects/src/script.js
@@ -14,6 +14,39 @@ const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+// Position
+// mesh.position.x = 0.7
+// mesh.position.y = - 0.6
+// mesh.position.z = 1
+mesh.position.set(0.7, -0.6, 1)
+
+// Scale
+// mesh.scale.x = 2 --> el doble de ancho
+// mesh.scale.y = 0.5 --> la mitad de alto
+// mesh.scale.z = 0.5 --> la mitad de profundidad
+
+// El resultado es un cubo "aplastado" que parece más un rectángulo plano y ancho.
+
+mesh.scale.set(2, 0.5, 0.5)
+
+// Rotation
+// reordenamos para que por ej. un personaje en el juego, mire primero de costado y luego abajo y arriba
+// si mira primero para abajo y luego de costado, va a quedar mal.
+
+// Primero reordenar y despues cambiar la rotacion 
+
+mesh.rotation.reorder ('YXZ') 
+mesh.rotation.x = Math.PI * 0.25
+mesh.rotation.y = 3.14159 * 0.25 // or Math.PI 
+
+// Quaternion
+
+
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
+
 /**
  * Sizes
  */
@@ -28,6 +61,8 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
+camera.lookAt()
 
 /**
  * Renderer
--- 04-transform-objects/vite.config.js
+++ 04-transform-objects/vite.config.js
@@ -1,5 +1,3 @@
-import restart from 'vite-plugin-restart'
-
 export default {
     root: 'src/', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
@@ -14,8 +12,5 @@ export default {
         emptyOutDir: true, // Empty the folder first
         sourcemap: true // Add sourcemap
     },
-    plugins:
-    [
-        restart({ restart: [ '../static/**', ] }) // Restart server on static file change
-    ],
+    plugins: [],
 }
